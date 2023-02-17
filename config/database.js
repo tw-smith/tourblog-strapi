d2abc268cbc0af7da41faa683b5f64c64e489f25
tw-smith <92607103+tw-smith@users.noreply.github.com>
2023-02-17 10:05:31 +0000

    adds production config files

--- config/database.js
+++ config/database.js
@@ -1,16 +1,10 @@
+const path = require('path');
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'sqlite',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      databse: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', ''),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: {
-        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-      },
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
